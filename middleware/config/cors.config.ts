67b586af8004c687c0efd7cb4347c9736e005e26
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-09-14 21:41:50 +0530

    kube

--- middleware/config/cors.config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
--- middleware/util/types.ts
+++
@@ -1,8 +0,0 @@
-export type ComparisonOperator = "=" | "!=" | "<" | "<=" | ">" | ">=";
-
-export type QueryOperation =
-  | { [key in ComparisonOperator]?: [QueryVariable, string | number] }
-  | { and?: QueryOperation[] }
-  | { or?: QueryOperation[] };
-
-export type QueryVariable = { var: string };
