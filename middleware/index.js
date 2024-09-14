b7589b1c9e956c0b7fafd8122db8606f4c9d0c2c
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-09-15 02:13:52 +0530

    create index js

---
+++ middleware/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let items = [];
+
+app.use(express.json());
+
+app.get('/items', (req, res) => {
+	res.json(items);
+});
+
+app.post('/items', (req, res) => {
+	const item = req.body;
+	items.push(item);
+	res.status(201).json(item);
+});
+
+app.get('/items/:id', (req, res) => {
+	const item = items.find((i) => i.id === parseInt(req.params.id));
+	if (item) {
+		res.json(item);
+	} else {
+		res.status(404).send('Item not found');
+	}
+});
+
+app.put('/items/:id', (req, res) => {
+	const itemIndex = items.findIndex((i) => i.id === parseInt(req.params.id));
+	if (itemIndex !== -1) {
+		items[itemIndex] = req.body;
+		res.json(items[itemIndex]);
+	} else {
+		res.status(404).send('Item not found');
+	}
+});
+
+app.delete('/items/:id', (req, res) => {
+	const itemIndex = items.findIndex((i) => i.id === parseInt(req.params.id));
+	if (itemIndex !== -1) {
+		items.splice(itemIndex, 1);
+		res.status(204).send();
+	} else {
+		res.status(404).send('Item not found');
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
+
